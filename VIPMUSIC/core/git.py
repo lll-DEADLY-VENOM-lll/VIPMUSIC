388cdeb66061bed4b223fc39a61bca75d80d519d
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-05-05 06:13:32 +0530

    Add copyright and license information to git.py
    Added copyright information and license details to git.py.

--- VIPMUSIC/core/git.py
+++ VIPMUSIC/core/git.py
@@ -1,3 +1,11 @@
+#
+# Copyright (C) 2021-2022 by KIRU-OP@Github, < https://github.com/KIRU-OP >.
+#
+# This file is part of < https://github.com/KIRU-OP/VIPMUSIC > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/KIRU-OP/VIPMUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
 import asyncio
 import shlex
 from typing import Tuple
