0be035a13875ea15d600ebe6a0f4537e12c8b3ac
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-05-05 06:06:54 +0530

    Refactor MongoDB initialization logic
    Refactor database initialization to handle fallback and user-provided MongoDB URIs with improved error handling.

--- VIPMUSIC/core/mongo.py
+++ VIPMUSIC/core/mongo.py
@@ -1,43 +1,69 @@
-# Copyright (C) 2024 by VISHAL-PANDEY@Github, < https://github.com/vishalpandeynkp1 >.
 #
-# This file is part of < https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO > project,
+# Copyright (C) 2021-2022 by KIRU-OP@Github, < https://github.com/KIRU-OP >.
+#
+# This file is part of < https://github.com/KIRU-OP/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO/blob/master/LICENSE >
+# Please see < https://github.com/KIRU-OP/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-#
 
+import sys
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
+from pymongo.errors import PyMongoError
 
 import config
-
 from ..logging import LOGGER
 
+# Fallback Database URL
 TEMP_MONGODB = "mongodb+srv://vishalpandeynkp:Bal6Y6FZeQeoAoqV@cluster0.dzgwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+def initialize_database():
+    global mongodb, pymongodb
 
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝙉𝙊𝘽𝙄𝙏𝘼 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
-    )
-    temp_client = Client(
-        "VIPMUSIC",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.VIPMUSIC
-    pymongodb = _mongo_sync_.VIPMUSIC
+    if config.MONGO_DB_URI is None:
+        LOGGER(__name__).warning(
+            "No MONGO_DB_URI found. Your bot will use the fallback NOBITA MUSIC database."
+        )
+        try:
+            # Use in_memory=True to avoid creating unnecessary session files
+            temp_client = Client(
+                "VIPMUSIC_TEMP",
+                bot_token=config.BOT_TOKEN,
+                api_id=config.API_ID,
+                api_hash=config.API_HASH,
+                in_memory=True,
+            )
+            temp_client.start()
+            info = temp_client.get_me()
+            username = info.username
+            temp_client.stop()
+
+            # Connecting to the fallback database using the bot's username
+            _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+            _mongo_sync_ = MongoClient(TEMP_MONGODB)
+            mongodb = _mongo_async_[username]
+            pymongodb = _mongo_sync_[username]
+            LOGGER(__name__).info(f"Connected to fallback database for bot: @{username}")
+
+        except Exception as e:
+            LOGGER(__name__).error(f"Failed to initialize fallback database: {e}")
+            sys.exit()
+    else:
+        try:
+            # Connecting to the user-provided MongoDB URI
+            _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+            _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+            
+            # Using 'VIPMUSIC' as the default database name
+            mongodb = _mongo_async_.VIPMUSIC
+            pymongodb = _mongo_sync_.VIPMUSIC
+            LOGGER(__name__).info("Custom MongoDB connection established successfully.")
+
+        except PyMongoError as e:
+            LOGGER(__name__).error(f"Invalid MONGO_DB_URI or connection error: {e}")
+            sys.exit()
+
+# Run the initialization
+initialize_database()
